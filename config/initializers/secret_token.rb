69845ea97dc079d3b8e383a809ec39caede54696
Kryss <kryss.col@gmail.com>
2011-03-11 16:35:37 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prolambda::Application.config.secret_token = '4f0ef784543283418c5a73c280e3eb21e05bcbc43de77fe45e1f3d0fa8fbb75e2e863955ac87ca272bf4a25506f4035f4af96c6c40c257963ea2049effb84765'
